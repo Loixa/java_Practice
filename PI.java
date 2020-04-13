a6381e752b5807e6ec6e66a6454939f16bdde519
Andy J Gomez Genao <andy.gomez.genao@gmail.com>
2020-04-13 15:08:50 -0400

    Add files via upload

---
+++ PI.java
@@ -0,0 +1,31 @@
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Loixa
+ * Find PI to the Nth Digit – Enter a number and have the program generate PI up
+ * to that many decimal places. Keep a limit to how far the program will go.
+ */
+public class PI {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        num = sc.nextInt();
+        double pI;
+        pI = Math.PI;
+        String n=("%."+num+"f");
+        System.out.format(n, pI);
+        System.out.println();
+
+        
+        
+        
+    }
+    
+}
